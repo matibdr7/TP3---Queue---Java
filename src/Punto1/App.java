86ff01bd750fda6a9753a421baf7d78dcf0320a5
@matibdr7 <matias.bdr7@gmail.com>
2024-10-10 01:48:43 -0300

    Base para Punto1

---
+++ src/Punto1/App.java
@@ -0,0 +1,15 @@
+/*
+Desarrollar un programa que simule un sistema de atención de clientes que hacen fila en una tienda.
+Implementar la clase cliente con los atributos (codCliente, aynCliente, mailCliente).
+Los clientes deben ingresar a la cola y esperar su turno para recibir la atención requerida. Mostrar los datos
+del cliente cuando sale de la cola. Al finalizar la “jornada” mostrar la cantidad de clientes cuyo apellido
+coincida con uno ingresado por teclado
+*/
+
+package Punto1;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+}
