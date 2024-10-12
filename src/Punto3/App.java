8d2b9a7cace9d11d280a393d58d2777d7dcbe6c7
@matibdr7 <matias.bdr7@gmail.com>
2024-10-11 23:37:59 -0300

    Base para punto 3

---
+++ src/Punto3/App.java
@@ -0,0 +1,16 @@
+/*
+Crear un programa para gestionar la cola de documentos en una impresora, de manera que se puedan
+encolar y desencolar trabajos de impresión. 
+
+En la cola se deben insertar objetos de tipo trabajoImpresion que poseen los atributos (codTrabajo, nomTrabajo, cantPagTrabajo).
+
+Al finalizar, el programa debe mostrar la cantidad de trabajos, cuya cantidad de páginas sea mayor o igual a un valor ingresado por teclado,que se imprimieron. 
+*/
+
+package Punto3;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+}
