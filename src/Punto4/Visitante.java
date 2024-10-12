09493fd90472aadae854971244206f33afac6545
@matibdr7 <matias.bdr7@gmail.com>
2024-10-12 01:08:22 -0300

    clase Visitante implementada

---
+++ src/Punto4/Visitante.java
@@ -0,0 +1,45 @@
+//Implementar la clase Visitante (idVisitante, aynVisitante, edad). 
+
+package Punto4;
+
+public class Visitante {
+    private int idVisitante;
+    private String aynVisitante;
+    private int Edad;
+
+    public Visitante() {
+    }
+
+    public Visitante(int idVisitante, String aynVisitante, int edad) {
+        this.idVisitante = idVisitante;
+        this.aynVisitante = aynVisitante;
+        Edad = edad;
+    }
+
+    public int getIdVisitante() {
+        return idVisitante;
+    }
+    public void setIdVisitante(int idVisitante) {
+        this.idVisitante = idVisitante;
+    }
+    public String getAynVisitante() {
+        return aynVisitante;
+    }
+    public void setAynVisitante(String aynVisitante) {
+        this.aynVisitante = aynVisitante;
+    }
+    public int getEdad() {
+        return Edad;
+    }
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Visitante [idVisitante=" + idVisitante + ", aynVisitante=" + aynVisitante + ", Edad=" + Edad + "]";
+    }
+
+    
+
+}
