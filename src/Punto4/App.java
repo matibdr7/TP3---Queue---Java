8eb9cbe35ce84dbf37f51bff1fdb93134b637831
@matibdr7 <matias.bdr7@gmail.com>
2024-10-12 00:54:15 -0300

    Base para Punto4

---
+++ src/Punto4/App.java
@@ -0,0 +1,25 @@
+/*
+Escribir un programa para gestionar el acceso de visitantes a los juegos en un parque de diversiones.
+
+Implementar la clase Visitante (idVisitante, aynVisitante, edad). 
+
+Los N (al azar) objetos visitantes deben ser insertados en una cola general para poder acceder a los juegos del parque. 
+A los fines de simplificar el ejercicio, se establece que solo existen dos juegos: montaña rusa y calesita. 
+
+Desde la cola general, los visitantes serán transferidos a dos colas específicas: una para la montaña rusa (apta para mayores de 12
+años) y otra para la calesita (apta para menores desde los 4 hasta los 12 años). 
+
+Los visitantes deben ser transferidos en grupos de 5 desde la cola general a las colas específicas de los juegos. 
+
+Mostrar los visitantes que van ingresando a los juegos, indicando a cuál juego acceden de acuerdo a su edad. 
+
+Al finalizar, informar la cantidad de visitantes menores de edad que tuvo el parque y la cantidad que ingresó en cada juego. 
+*/
+
+package Punto4;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+}
